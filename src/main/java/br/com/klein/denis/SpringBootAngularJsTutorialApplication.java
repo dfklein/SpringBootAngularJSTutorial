eb6376c015b29b161f2af733dd7024a2bca6bb59
dklei <dklei@DESKTOP-3A2BQ8A>
2017-06-22 22:44:35 -0300

    Commit inicial

---
+++ src/main/java/br/com/klein/denis/SpringBootAngularJsTutorialApplication.java
@@ -0,0 +1,12 @@
+package br.com.klein.denis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAngularJsTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAngularJsTutorialApplication.class, args);
+	}
+}
