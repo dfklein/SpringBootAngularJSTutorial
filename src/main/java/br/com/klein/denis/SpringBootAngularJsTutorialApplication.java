572f2aa8a4bc59e3f5949bbbace16eda11071f27
dklei <dklei@DESKTOP-0R8000F>
2017-07-02 23:13:30 -0300

    Finalização da aula 3 (exemplo de requisição Ajax, mapeamento de rota no controller, thymeleaf adicionado)

--- src/main/java/br/com/klein/denis/SpringBootAngularJsTutorialApplication.java
+++ src/main/java/br/com/klein/denis/SpringBootAngularJsTutorialApplication.java
@@ -2,11 +2,26 @@ package br.com.klein.denis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class SpringBootAngularJsTutorialApplication {
+public class SpringBootAngularJsTutorialApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootAngularJsTutorialApplication.class, args);
 	}
+	
+//	@Configuration
+//	public static class MvcConfig extends WebMvcConfigurerAdapter {
+//		
+//		@Override
+//		public void addViewControllers(ViewControllerRegistry registry) {
+//			registry.addRedirectViewController("/", "/SpringBootAngularJSTutorial"); // redirecionamento, tipo um filtro.
+//		}
+//		
+//	}
+	
 }
